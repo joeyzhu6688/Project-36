acb5f9ab29deb9dd9fed8b97dedeb442a47ea6c4
joeyzhu6688 <87821978+joeyzhu6688@users.noreply.github.com>
2021-12-18 18:05:07 -0600

    Add files via upload

--- Question.js
+++ Question.js
@@ -22,22 +22,22 @@ class Question {
   }
 
   display(){
-    this.title.html("MyQuiz Game");
+    this.title.html("My Riddle Game");
     this.title.position(350, 0);
 
     //this.question("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
     // this.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    this.question.html("Question:- What has to be broken before you can use it? " );
     // question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
 
     this.question.position(150, 80);
-    this.option1.html("1: Everyone " );
+    this.option1.html("1: Light Stick " );
     this.option1.position(150, 100);
-    this.option2.html("2: Envelope" );
+    this.option2.html("2: Egg" );
     this.option2.position(150, 120);
-    this.option3.html("3: Estimate " );
+    this.option3.html("3: Can Of Beans " );
     this.option3.position(150, 140);
-    this.option4.html("4: Example" );
+    this.option4.html("4: FireWood" );
     this.option4.position(150, 160);
 
     this.input1.position(150, 230);
